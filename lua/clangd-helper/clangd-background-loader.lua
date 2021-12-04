b02a57326c87be8c3287ec67758d92480c8f1133
codewdy <codewdy@gmail.com>
2021-12-04 09:37:21 +0800

    add source dir for filtering the inputs

--- lua/clangd-helper/clangd-background-loader.lua
+++ lua/clangd-helper/clangd-background-loader.lua
@@ -1,7 +1,8 @@
 local utils = require("clangd-helper/utils")
 
 local config = {
-  thread_num = 5
+  thread_num = 5,
+  configure_file = '.root'
 }
 
 local file_handler = {
@@ -55,22 +56,57 @@ local function handle_client(client)
   else
     return
   end
-  utils.async_walk(client.config.root_dir, function(err, filelist)
+  utils.async_read(client.config.root_dir .. '/' .. config.configure_file, vim.schedule_wrap(function(err, data)
     if err then
-      print("ERROR")
+      print("No configure_file was found, skip")
       return
     end
-    local processor_args = {
-      client_id = client.id,
-      filelist = filelist,
-      ptr = 1,
-      processed = 0,
-      error = 0
-    }
-    for i=1,config.thread_num do
-      load_processor(processor_args)
+    local status, result = pcall(vim.fn.json_decode, data)
+    xxx = status
+    if not status then
+      print("configure_file cannot be parsed, skip")
+      return
     end
-  end)
+    local source_dir = result.source_dir
+    if source_dir == nil then
+      print("no source_dir is configured, skip")
+      return
+    end
+    local absolute_source_dir = {}
+    for _,s in ipairs(source_dir) do
+      if string.sub(s, 1, 1) == '/' then
+        table.insert(absolute_source_dir, vim.loop.fs_realpath(s) .. '/')
+      else
+        table.insert(absolute_source_dir,
+            vim.loop.fs_realpath(client.config.root_dir .. '/' .. s) .. '/')
+      end
+    end
+    utils.async_walk(client.config.root_dir, function(err, filelist)
+      if err then
+        print("file list is not found, check rg is installed")
+        return
+      end
+      local filtered_filelist = {}
+      for _,filename in ipairs(filelist) do
+        for _,s in ipairs(absolute_source_dir) do
+          if string.sub(filename, 1, #s) == s then
+            table.insert(filtered_filelist, filename)
+            break
+          end
+        end
+      end
+      local processor_args = {
+        client_id = client.id,
+        filelist = filtered_filelist,
+        ptr = 1,
+        processed = 0,
+        error = 0
+      }
+      for i=1,config.thread_num do
+        load_processor(processor_args)
+      end
+    end)
+  end))
 end
 
 local function on_publish_diagnostics(_, result, ctx, config)
