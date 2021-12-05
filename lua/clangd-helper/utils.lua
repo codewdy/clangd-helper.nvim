28c5c536f5f010927f05eea52437e05b51b69e9c
codewdy <codewdy@gmail.com>
2021-12-05 14:26:29 +0800

    fix utils

--- lua/clangd-helper/utils.lua
+++ lua/clangd-helper/utils.lua
@@ -17,7 +17,7 @@ local function async_walk(dir, callback)
 
   local stdout = vim.loop.new_pipe(false)
   handle = vim.loop.spawn('rg', {
-      args = {'--type', 'cpp', '--files', dir},
+      args = {'--type', 'cpp', '--files', vim.loop.fs_realpath(dir)},
       stdio = {nil, stdout, nil}
     },
     vim.schedule_wrap(function()
@@ -54,7 +54,54 @@ local function async_read(filename, callback)
   end)
 end
 
+local function string_split(str, seps)
+  local sep = seps[1]
+  for _,s in ipairs(seps) do
+    if string.find(str, s, 1, true) ~= nil then
+      sep = s
+    end
+  end
+  local ptr = 1
+  local result = {}
+  while true do
+    local b, e = string.find(str, sep, ptr, true)
+    if b == nil then
+      table.insert(result, string.sub(str, ptr))
+      return result
+    else
+      table.insert(result, string.sub(str, ptr, b - 1))
+      ptr = e + 1
+    end
+  end
+end
+
+local function string_split_with_loc(str, seps)
+  local sep = seps[1]
+  for _,s in ipairs(seps) do
+    if string.find(str, s, 1, true) ~= nil then
+      sep = s
+    end
+  end
+  local ptr = 1
+  local result = {}
+  local loc = {}
+  while true do
+    local b, e = string.find(str, sep, ptr, true)
+    if b == nil then
+      table.insert(result, string.sub(str, ptr))
+      table.insert(loc, ptr)
+      return result, loc
+    else
+      table.insert(result, string.sub(str, ptr, b - 1))
+      table.insert(loc, ptr)
+      ptr = e + 1
+    end
+  end
+end
+
 return {
   async_walk = async_walk,
-  async_read = async_read
+  async_read = async_read,
+  string_split = string_split,
+  string_split_with_loc = string_split_with_loc
 }
