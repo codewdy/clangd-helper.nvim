bdc2ecfb49210061534e9f5d8f7bfc5754796ef7
codewdy <codewdy@gmail.com>
2021-12-06 23:11:49 +0800

    remove unused thread limit.

--- lua/clangd-helper/clangd-background-loader.lua
+++ lua/clangd-helper/clangd-background-loader.lua
@@ -1,38 +1,18 @@
 local utils = require("clangd-helper/utils")
 
 local config = {
-  thread_num = 5,
   configure_file = '.root'
-}
-
-local file_handler = {
 }
 
 local handled_client = {
 }
 
-local function load_processor(processor_args)
-  local idx = processor_args.ptr
-  if idx > #processor_args.filelist then
-    return
-  end
-  processor_args.ptr = idx + 1
-  local filename = processor_args.filelist[idx]
-  local client_id = processor_args.client_id
+local function clangd_load(client, filename)
   utils.async_read(filename, function (err, data)
     if err ~= nil then
-      processor_args.error = processor_args.error + 1
-      print("Processing:", processor_args.processed, '/', processor_args.error, '/', #processor_args.filelist)
-      load_processor(processor_args)
+      require("log").info("Processing:", processor_args.processed, '/', processor_args.error, '/', #processor_args.filelist)
       return
     end
-    local handler_name = tostring(client_id) .. ':file://' .. filename
-    file_handler[handler_name] = function()
-      processor_args.processed = processor_args.processed + 1
-      print("Processing:", processor_args.processed, '/', processor_args.error, '/', #processor_args.filelist, '\n')
-      load_processor(processor_args)
-    end
-    local client = vim.lsp.get_client_by_id(client_id)
     local params = {
       textDocument = {
         version = 0,
@@ -41,9 +21,7 @@ local function load_processor(processor_args)
         text = data
       }
     }
-    if client.notify('textDocument/didOpen', params) then
-      file_handler[handler_name] = nil
-    end
+    client.notify('textDocument/didOpen', params)
   end)
 end
 
@@ -95,43 +73,15 @@ local function handle_client(client)
           end
         end
       end
-      local processor_args = {
-        client_id = client.id,
-        filelist = filtered_filelist,
-        ptr = 1,
-        processed = 0,
-        error = 0
-      }
-      for i=1,config.thread_num do
-        load_processor(processor_args)
+      for _,filename in ipairs(filtered_filelist) do
+        clangd_load(client, filename)
       end
     end)
   end))
 end
 
-local function on_publish_diagnostics(_, result, ctx, config)
-  local client_id = ctx.client_id
-  local uri = result.uri
-
-  local handler_name = tostring(client_id) .. ':' .. uri
-
-  local hanlder = file_handler[handler_name]
-  file_handler[handler_name] = nil
-
-  if hanlder ~= nil then
-    handler()
-  end
-end
-
 local function on_attach(client, bufnr)
   handle_client(client)
-end
-
-local function on_publish_diagnostics_wapper(func)
-  return function(...)
-    on_publish_diagnostics(...)
-    func(...)
-  end
 end
 
 local function setup(cfg)
@@ -140,6 +90,5 @@ end
 
 return {
   on_attach = on_attach,
-  on_publish_diagnostics_wapper = on_publish_diagnostics_wapper,
   setup = setup
 }
